cfe98b3e34562a5e8192d8904dce87a2ab90607e
David Halewood <davidnhalewood@hotmail.com>
2017-09-02 10:51:35 +0100

    updating README

---
+++ chapter2/chap2challenges.rb
@@ -0,0 +1,10 @@
+# Hours in a year
+puts (24 * 365)
+
+# Minutes in a decade
+puts (60 * 24 * 365 * 10)
+
+#My age in seconds
+puts (365 * 24 * 60 * 60 * 35)
+puts "_____"
+puts (Time.now - Time.new(1983/01/26))
