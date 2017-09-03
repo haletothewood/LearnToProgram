ee0379352c792f3c0ba4bd63159137f1442abcba
David Halewood <davidnhalewood@hotmail.com>
2017-09-03 10:18:37 +0100

    adding chapter 8

---
+++ chapter8/arraysort.rb
@@ -0,0 +1,24 @@
+def array_sort
+
+	puts "Please being entering your chose words, one per line followed by enter."
+	puts "When you're done please press enter on an empty line."
+	puts "Ready: > "
+
+	x = gets.chomp
+	arr = []
+
+	while x != ''
+		x = gets.chomp
+		arr << x
+	end
+
+	arr.sort!
+
+	arr.each do |y|
+		if y != ''
+			puts y
+		end
+	end
+end
+
+array_sort
